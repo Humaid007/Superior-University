81a8875d83c1d8dfc54f10f521dbb81a6eb0758b
Humaid007 <humaidhussain107@gmail.com>
2024-12-09 05:28:53 -0800

    Create musiclibrary.py

---
+++ AI Lab/Task/project/musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "sunrise": "https://www.youtube.com/watch?v=f9acDGddKr0",
+    "moon": "https://www.youtube.com/watch?v=ilNt2bikxDI",
+    "peach": "https://www.youtube.com/watch?v=AX6OrbgS8lI",
+    "apple": "https://www.youtube.com/watch?v=KHgyqE4MnxI",
+}
